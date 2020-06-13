180e0b96626e2590ef30eb39571c995a2cf6543c
sanjida nawrin <sanjidanawrin123@gmail.com>
2020-06-13 10:06:07 -0400

    app not working have to fix it

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/mongoHeadlines", {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
